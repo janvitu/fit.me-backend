742965cf3d10c067da9150f23e5d2d161f7d0082
Jan Hála <honza.hala@gmail.com>
2021-11-14 18:05:01 +0100

    Added sportsman registration and login

--- src/index.js
+++ src/index.js
@@ -1,31 +1,75 @@
-import dotenv from "dotenv-flow";
-import express from "express";
-import cors from "cors";
-import mysql from "mysql";
+import dotenv from 'dotenv-flow';
+import express from 'express';
+import cors from 'cors';
+import { ApolloServer, gql } from 'apollo-server-express';
+import { ApolloServerPluginLandingPageGraphQLPlayground } from 'apollo-server-core';
+
+import { getConnection } from './libs/connection';
+
+import rootResolver from './modules/rootResolver';
 
 dotenv.config();
 
-const mysqlConnection = mysql.createConnection({
-	host: process.env.MYSQL_HOST,
-	port: process.env.MYSQL_PORT,
-	user: process.env.MYSQL_USER,
-	password: process.env.MYSQL_PASSWORD,
-	database: process.env.MYSQL_DATABASE,
-});
+const typeDefs = gql`
+  type Query {
+    todo: String!
+  }
+  type Sportsman {
+    name: String!
+    surname: String!
+    email: String!
+    password: String!
+    phone: String
+  }
+  type AuthInfo {
+    sportsman: Sportsman!
+    token: String!
+  }
+  type Mutation {
+    sportsmanSignIn(email: String!, password: String!): AuthInfo!
+    sportsmanSignUp(name: String!, surname: String!, email: String!, password: String!): Boolean
+    coachSignUp(name: String!, surname: String!, email: String!, password: String!, username: String!, phone: String!, vat_number: String!): Boolean
+  }
+`;
 
-const app = express();
-app.use(cors());
+const main = async () => {
+  const app = express();
 
-mysqlConnection.connect((err) => {
-	if (err) {
-		console.log({ err });
-		return;
-	}
-	console.log("connected as id " + mysqlConnection.threadId);
-});
+  app.disable('x-powered-by');
+  app.use(cors());
 
-const port = process.env.PORT || 4000;
+  let dbConnection = null;
 
-app.listen(port, () => {
-	console.info(`Server started at http://localhost:${port}`);
-});
+  const apolloServer = new ApolloServer({
+    typeDefs,
+    resolvers: rootResolver,
+    context: async ({ req, res }) => {
+      if (!dbConnection) {
+        dbConnection = await getConnection();
+      }
+      const auth = req.headers.Authorization || '';
+
+      return {
+        req,
+        res,
+        dbConnection,
+        auth,
+      };
+    },
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app, cors: false });
+
+  const port = process.env.PORT || 4000;
+
+  app.get('/', (_, res) => res.redirect('/graphql'));
+
+  app.listen(port, () => {
+    console.info(`Server started at http://localhost:${port}/graphql`);
+  });
+};
+
+main();
--- src/modules/user/mutation.js
+++ src/modules/user/mutation.js
@@ -1,14 +1,65 @@
+import * as argon2 from 'argon2';
 import { createToken } from '../../libs/token';
 
-export const signin = async (_, { email, password }) => {
-  if (!(email === 'a@a.com' && password === 'pass')) {
-    throw Error('Invalid username or password');
+export const sportsmanSignIn = async (_, { email, password }, { dbConnection }) => {
+  const dbResponse = await dbConnection.query(
+    'SELECT * FROM sportsman WHERE email = ?',
+    [email],
+  );
+  const sportsman = dbResponse[0];
+  const token = createToken({ id: sportsman.id });
+
+  if (await argon2.verify(sportsman.password, password)) {
+    const token = createToken({ id: sportsman.id });
+    return {
+      sportsman: { ...sportsman },
+      token,
+    };
+  }
+};
+
+export const sportsmanSignUp = async (
+  _,
+  { name, surname, email, password },
+  { dbConnection },
+) => {
+  const existingSportsman = (
+    await dbConnection.query("SELECT * FROM sportsman WHERE email = ?", [email])
+  )[0];
+
+  if (existingSportsman) {
+    throw new Error('This email was already taken.');
   }
 
-  const mockUser = { id: 1, email };
-  const token = createToken(mockUser);
+  const passwordHash = await argon2.hash(password);
 
-  return {
-    token,
-  };
+  const dbResponse = await dbConnection.query(
+    'INSERT INTO sportsman (name, surname, email, password) VALUES (?, ?, ?, ?);',
+    [name, surname, email, passwordHash],
+  );
+
+  return true;
+};
+
+export const coachSignUp = async (
+  _,
+  { name, surname, email, password, username, phone, vat_number  },
+  { dbConnection },
+) => {
+  const existingCoach = (
+    await dbConnection.query("SELECT * FROM coach WHERE email = ?", [email])
+  )[0];
+
+  if (existingCoach) {
+    throw new Error('This email was already taken.');
+  }
+
+  const passwordHash = await argon2.hash(password);
+
+  const dbResponse = await dbConnection.query(
+    'INSERT INTO coach (name, surname, email, password, username, phone, vat_number) VALUES (?, ?, ?, ?, ?, ?, ?);',
+    [name, surname, email, passwordHash, username, phone, vat_number],
+  );
+
+  return true;
 };
